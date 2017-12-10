b279d1c6e6930dbfb72e1cf234bb7ed00eacf629
hans <12345678@qq.com>
2017-12-10 19:15:51 +0800

    完成启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -7,8 +7,32 @@ define(['jquery', 'template'], function ($, template) {
         dataType: 'json',
         success: function (data) {
             var html = template('teacherTpl', { list: data.result });
-            $('#teacherInfo').html(html)
-        }
-    })
+            $('#teacherInfo').html(html);
 
+            // 讲师启用/注销功能
+            $('.disbleOrEnable').click(function () {
+                var that = this;
+                var td = $(this).parent('td');
+                var tcId = td.attr('data-tcId');
+                var tcStatus = td.attr('data-tcStatus');
+                $.ajax({
+                    type: 'post',
+                    url: '/api/teacher/handle',
+                    dataType: 'json',
+                    data: { tc_id: tcId, tc_status: tcStatus },
+                    success: function (data) {
+                        if (data.code == 200) {
+                            td.attr('data-tcStatus', data.result.tc_status);
+                            if (data.result.tc_status == 0) {
+                                $(that).text('注销')
+                            } else {
+                                $(that).text('启用')
+
+                            }
+                        }
+                    }
+                })
+            });
+        }
+    });
 });
