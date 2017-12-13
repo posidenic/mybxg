72536d92c32b8cab43ffcc1432684757a54e2f2c
hans <12345678@qq.com>
2017-12-13 13:59:29 +0800

    完成个人中心表单提交

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'ckeditor', 'uploadify', 'region','datepicker','language'], function ($, template, CKEDITOR) {
+define(['jquery', 'template', 'ckeditor', 'uploadify', 'region', 'datepicker', 'language', 'validate', 'form'], function ($, template, CKEDITOR) {
     // 调用接口，获取个人信息
     $.ajax({
         type: 'get',
@@ -29,12 +29,44 @@ define(['jquery', 'template', 'ckeditor', 'uploadify', 'region','datepicker','la
             });
 
             // 处理富文本
-            CKEDITOR.replace('editor',{
-                toolbarGroups : [
-                  { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-                  { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] }
+            CKEDITOR.replace('editor', {
+                toolbarGroups: [
+                    { name: 'clipboard', groups: ['clipboard', 'undo'] },
+                    { name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] }
                 ]
-              })
+            });
+
+            // 处理表单提交
+            $('#settingsForm').validate({
+                sendForm: false,
+                valid: function () {
+                    // 获取家乡信息
+                    var p = $('#p').find('option:selected').text();
+                    var c = $('#c').find('option:selected').text();
+                    var d = $('#d').find('option:selected').text();
+                    var hometown = p+'|'+c+'|'+d;
+
+                    // 同步富文本内容
+                    for (var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+
+                    $(this).ajaxSubmit({
+                        type: 'post',
+                        url: '/api/teacher/modify',
+                        data:{
+                            tc_hometown:hometown
+                        },
+                        dataType: 'json',
+                        success: function (data) {
+                            if (data.code == 200) {
+                                //   修改成功后，重新刷新当前页面
+                                location.reload();
+                            }
+                        }
+                    })
+                }
+            })
 
         }
     })
